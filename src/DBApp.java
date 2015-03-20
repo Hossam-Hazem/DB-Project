4844eb7745a4cb8a8cec19e870beda5b79394fd8
kareem <al3ameeeel94@gmail.com>
2015-03-20 08:55:20 +0200

    added required methods

--- src/DBApp.java
+++ src/DBApp.java
@@ -1,4 +1,66 @@
+import java.util.Hashtable;
+import java.util.Iterator;
+
 
 public class DBApp implements RequiredMethods {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createTable(String strTableName,
+			Hashtable<String, String> htblColNameType,
+			Hashtable<String, String> htblColNameRefs, String strKeyColName)
+			throws DBAppException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createIndex(String strTableName, String strColName)
+			throws DBAppException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertIntoTable(String strTableName,
+			Hashtable<String, String> htblColNameValue) throws DBAppException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteFromTable(String strTableName,
+			Hashtable<String, String> htblColNameValue, String strOperator)
+			throws DBEngineException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Iterator selectValueFromTable(String strTable,
+			Hashtable<String, String> htblColNameValue, String strOperator)
+			throws DBEngineException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator selectRangeFromTable(String strTable,
+			Hashtable<String, String> htblColNameRange, String strOperator)
+			throws DBEngineException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveAll() throws DBEngineException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
