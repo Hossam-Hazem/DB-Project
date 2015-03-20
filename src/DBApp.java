5489a8d58f5e9215624c70d98e0c1cc886d73db8
kareem <al3ameeeel94@gmail.com>
2015-03-20 10:49:33 +0200

    MetaData Filing

--- src/DBApp.java
+++ src/DBApp.java
@@ -1,13 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
 
-
 public class DBApp implements RequiredMethods {
+	public static void main(String[] args) {
+		Hashtable<String, String> htblColNameType = new Hashtable<String, String>();
+		htblColNameType.put("col1", "str");
+		htblColNameType.put("col2", "int");
+		htblColNameType.put("col3", "int");
+		htblColNameType.put("col4", "str");
+
+		Hashtable<String, String> htblColNameRefs = new Hashtable<String, String>();
+		htblColNameRefs.put("col1", "table1.id");
+
+		DBApp app = new DBApp();
+		try {
+			app.createTable("kareem", htblColNameType, htblColNameRefs, "col1");
+		} catch (DBAppException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -16,21 +39,56 @@ public class DBApp implements RequiredMethods {
 			Hashtable<String, String> htblColNameRefs, String strKeyColName)
 			throws DBAppException {
 		// TODO Auto-generated method stub
-		
+
+		String metaInfo = "Table Name, Column Name, Column Type, Key, Indexed, References"
+				+ "\n";
+
+		Enumeration ColNames = htblColNameType.keys();
+		while (ColNames.hasMoreElements()) {
+			String ColName = (String) ColNames.nextElement();
+			metaInfo += strTableName + ", " + ColName + ","
+					+ htblColNameType.get(ColName) + ", "
+					+ ((ColName.equals(strKeyColName)) ? "true" : "false")
+					+ ", "
+					+ ((ColName.equals(strKeyColName)) ? "true" : "false")
+					+ ", " + htblColNameRefs.get(ColName) + "\n";
+		}
+		try {
+			writeFile(strTableName + ".meta", metaInfo);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void writeFile(String fileName, String text) throws IOException {
+
+		BufferedWriter output = new BufferedWriter(new FileWriter(fileName));
+		output.write(text);
+		output.close();
+	}
+
+	public void readFile(String fileName) throws IOException {
+
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		br.readLine();
+		br.close();
+
 	}
 
 	@Override
 	public void createIndex(String strTableName, String strColName)
 			throws DBAppException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void insertIntoTable(String strTableName,
 			Hashtable<String, String> htblColNameValue) throws DBAppException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -38,7 +96,7 @@ public class DBApp implements RequiredMethods {
 			Hashtable<String, String> htblColNameValue, String strOperator)
 			throws DBEngineException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -60,7 +118,7 @@ public class DBApp implements RequiredMethods {
 	@Override
 	public void saveAll() throws DBEngineException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
