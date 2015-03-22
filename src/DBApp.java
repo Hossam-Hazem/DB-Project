f710a801eecc9d113007c5288b8e17d8618d1c56
omaradelahmed <omar.adelahmed@student.guc.edu.eg>
2015-03-22 20:43:10 +0200

    rearrange folders and directories

--- src/DBApp.java
+++ src/DBApp.java
@@ -21,70 +21,89 @@ public class DBApp {
 			Hashtable<String, String> htblColNameType,
 			Hashtable<String, String> htblColNameRefs, String strKeyColName)
 			throws DBAppException, IOException {
-		if(alreadyExist(strTableName)==false){
-		FileReader fileReader = new FileReader("data/metadata.csv");
-		BufferedReader br = new BufferedReader(fileReader);
-		String x = br.readLine();
+		if (alreadyExist(strTableName) == false) {
+			FileReader fileReader = new FileReader("data/metadata.csv");
+			BufferedReader br = new BufferedReader(fileReader);
+			String x = br.readLine();
 
-		FileWriter fileWriter = new FileWriter("data/metadata.csv", true);
+			FileWriter fileWriter = new FileWriter("data/metadata.csv", true);
 
-		if (x == null) {
-			fileWriter
-					.append("Table Name, Column Name, Column Type, Key, Indexed, References");
-			fileWriter.append("\n");
-		}
+			if (x == null) {
+				fileWriter
+						.append("Table Name, Column Name, Column Type, Key, Indexed, References");
+				fileWriter.append("\n");
+			}
 
-		Set set = htblColNameType.entrySet();
-		Iterator it = set.iterator();
-		while (it.hasNext()) {
-			Map.Entry entry = (Map.Entry) it.next();
-			String temp = strTableName + ", " + (String) entry.getKey() + ", "
-					+ (String) entry.getValue() + ", "
-					+ (entry.getKey().equals(strKeyColName) ? "true" : "false")
-					+ ", " + "false" + ", "
-					+ htblColNameRefs.get(entry.getKey()) + "\n";
+			Set set = htblColNameType.entrySet();
+			Iterator it = set.iterator();
+			while (it.hasNext()) {
+				Map.Entry entry = (Map.Entry) it.next();
+				String temp = strTableName
+						+ ", "
+						+ (String) entry.getKey()
+						+ ", "
+						+ (String) entry.getValue()
+						+ ", "
+						+ (entry.getKey().equals(strKeyColName) ? "true"
+								: "false") + ", " + "false" + ", "
+						+ htblColNameRefs.get(entry.getKey()) + "\n";
 
-			fileWriter.append(temp);
-			System.out.println(entry.getKey() + " : " + entry.getValue());
-		}
-		br.close();
-		fileWriter.close();
+				fileWriter.append(temp);
+				System.out.println(entry.getKey() + " : " + entry.getValue());
+			}
+			br.close();
+			fileWriter.close();
 		}
 		makeTable(strTableName);
 	}
-	
+
 	private static void makeTable(String strTableName) throws IOException {
 		Table x = new Table(strTableName);
-		String path = "data/tables/"+strTableName+".bin";
+		String path = "data/tables/" + strTableName + "/" + strTableName
+				+ ".bin";
+		// make folder containing all table info
 		File saveDir = new File("data/tables");
-		if(!saveDir.exists()){
-		    saveDir.mkdirs(); 
+		if (!saveDir.exists()) {
+			saveDir.mkdirs();
+		}
+		// make pages directory inside table folder
+		saveDir = new File("data/tables/" + strTableName + "/" + "pages");
+		if (!saveDir.exists()) {
+			saveDir.mkdirs();
+		}
+		// make hashtable directory inside table folder
+		saveDir = new File("data/tables/" + strTableName + "/" + "hashtable");
+		if (!saveDir.exists()) {
+			saveDir.mkdirs();
+		}
+		// make BTree directory inside table folder
+		saveDir = new File("data/tables/" + strTableName + "/" + "BTree");
+		if (!saveDir.exists()) {
+			saveDir.mkdirs();
 		}
 		/*
-		FileOutputStream fs = new FileOutputStream(path);
-		ObjectOutputStream os = new ObjectOutputStream(fs);
-		os.writeObject(x);
-		os.close();
-		fs.close();
-		*/
+		 * FileOutputStream fs = new FileOutputStream(path); ObjectOutputStream
+		 * os = new ObjectOutputStream(fs); os.writeObject(x); os.close();
+		 * fs.close();
+		 */
 		serialize(path, x);
 	}
 
-	public static boolean alreadyExist(String strTableName) throws IOException{
+	public static boolean alreadyExist(String strTableName) throws IOException {
 		boolean found = false;
 		String currentLine = "";
-		FileReader fileReader= new FileReader("data/metadata.csv");
+		FileReader fileReader = new FileReader("data/metadata.csv");
 		BufferedReader br = new BufferedReader(fileReader);
 		while ((currentLine = br.readLine()) != null) {
-			String [] result= currentLine.split(", ");
-			if(result[0].equals(strTableName)){
+			String[] result = currentLine.split(", ");
+			if (result[0].equals(strTableName)) {
 				found = true;
 				break;
 			}
 		}
 		br.close();
 		return found;
-		
+
 	}
 
 	public static void createIndex(String strTableName, String strColName)
@@ -118,120 +137,125 @@ public class DBApp {
 
 	// Under Construction
 	public static void insertIntoTable(String strTableName,
-			Hashtable<String, String> htblColNameValue) throws DBAppException, ClassNotFoundException, IOException {
-		String path = "data/tables/" + strTableName+".bin";
-		Table x = (Table)deserialize(path);
-		if(x.getAllPages().size() == 0){
+			Hashtable<String, String> htblColNameValue) throws DBAppException,
+			ClassNotFoundException, IOException {
+		String path = "data/tables/" + strTableName + "/" + strTableName
+				+ ".bin";
+		Table x = (Table) deserialize(path);
+		if (x.getAllPages().size() == 0) {
 			x.createPage();
-			System.out.println("yup");
+			System.out.println("First page intialized");
 		}
-		
-		String lastPage = x.getAllPages().get(x.getAllPages().size()-1);
+
+		String lastPage = x.getAllPages().get(x.getAllPages().size() - 1);
 		System.out.println(lastPage);
 		System.out.println(lastPage);
-		path ="data/pages/"+strTableName+"/"+lastPage+".bin";
-		Page lastPageinTable = (Page)deserialize(path);
-		if(lastPageinTable.getRowsCounter() < lastPageinTable.getMaximumRowsCountinPage()){
+		path = "data/tables/" + strTableName + "/" + "pages/" + lastPage
+				+ ".class";
+		Page lastPageinTable = (Page) deserialize(path);
+		if (!lastPageinTable.isFull()) {
 			lastPageinTable.addRecord(htblColNameValue);
-			lastPageinTable.setRowsCounter(lastPageinTable.getRowsCounter()+1);
-		}
-		else{
-			x.createPage(); //already added in the method to the array
-			lastPage = x.getAllPages().get(x.getAllPages().size()-1);
-			System.out.println("*"+lastPage+"*");
-			path ="data/pages/"+strTableName+"/"+lastPage+".bin";
-			lastPageinTable = (Page)deserialize(path);
+			lastPageinTable
+					.setRowsCounter(lastPageinTable.getRowsCounter() + 1);
+		} else {
+			x.createPage(); // already added in the method to the array
+			lastPage = x.getAllPages().get(x.getAllPages().size() - 1);
+			System.out.println("New page created");
+			path = "data/tables/" + strTableName + "/" + "pages/" + lastPage
+					+ ".class";
+			lastPageinTable = (Page) deserialize(path);
 			lastPageinTable.addRecord(htblColNameValue);
-			lastPageinTable.setRowsCounter(lastPageinTable.getRowsCounter() + 1);
-			System.out.println("upu");
-			
+			lastPageinTable
+					.setRowsCounter(lastPageinTable.getRowsCounter() + 1);
+
 		}
-		path = "data/tables/" + strTableName+".bin";
+		path = "data/tables/" + strTableName + "/" + strTableName + ".bin";
 		serialize(path, x);
-		path = "data/pages/" + strTableName+"/"+lastPage+".bin";
+		path = "data/tables/" + strTableName + "/" + "pages/" + lastPage
+				+ ".class";
 		serialize(path, lastPageinTable);
-		
-		
-		
-		
-		
+
 	}
-	
-	public static Object deserialize(String path) throws IOException, ClassNotFoundException{
+
+	public void deleteFromTable(String strTableName,
+			Hashtable<String, String> htblColNameValue, String strOperator)
+			throws DBEngineException {
+
+	}
+
+	public static Object deserialize(String path) throws IOException,
+			ClassNotFoundException {
 		FileInputStream fi = new FileInputStream(path);
 		ObjectInputStream os = new ObjectInputStream(fi);
-		Object x =  os.readObject();
-		//System.out.println(((Page)x).getRecords().get(0));
+		Object x = os.readObject();
+		// System.out.println(((Page)x).getRecords().get(0));
 		os.close();
 		fi.close();
 		return x;
 	}
-	
-	public static void  serialize(String path, Object x) throws IOException{
+
+	public static void serialize(String path, Object x) throws IOException {
 		FileOutputStream fs = new FileOutputStream(path);
 		ObjectOutputStream os = new ObjectOutputStream(fs);
 		os.writeObject(x);
 		os.close();
 		fs.close();
 	}
-	
 
-	public static void main(String[] args) throws IOException, DBAppException, ClassNotFoundException {
+	public static void main(String[] args) throws IOException, DBAppException,
+			ClassNotFoundException {
 		/*
-		  Hashtable<String, String> htblColNameType = new Hashtable<String,
-		  String>(); htblColNameType.put("col1", "str");
-		  htblColNameType.put("col2", "int"); htblColNameType.put("col3",
-		  "int"); htblColNameType.put("col4", "str");
-		  
-		  Hashtable<String, String> htblColNameRefs = new Hashtable<String,
-		  String>(); htblColNameRefs.put("col1", "table1.id");
-		  
-		  createTable("testAll2", htblColNameType, htblColNameRefs, "col2");
+		 * Hashtable<String, String> htblColNameType = new Hashtable<String,
+		 * String>(); htblColNameType.put("col1", "str");
+		 * htblColNameType.put("col2", "int"); htblColNameType.put("col3",
+		 * "int"); htblColNameType.put("col4", "str");
+		 * 
+		 * Hashtable<String, String> htblColNameRefs = new Hashtable<String,
+		 * String>(); htblColNameRefs.put("col1", "table1.id");
+		 * 
+		 * createTable("testAll6", htblColNameType, htblColNameRefs, "col2");
 		 */
-		//createIndex("testAll", "col3");
+		// createIndex("testAll", "col3");
 
 		// Clean csv file
 		/*
 		 * FileWriter fileWriter = new FileWriter("data/metadata.csv");
 		 * fileWriter.append("");
 		 */
-		//System.out.println(alreadyExist("test10"));
-		
-		//makeTable("test4")
-		  /*
-		FileInputStream fi = new FileInputStream("data/tables/test4.bin");
-		ObjectInputStream os = new ObjectInputStream(fi);
-		Table x =  (Table)os.readObject();
-		System.out.println(x.getTableName());
-		os.close();
-		fi.close();
-		*/
-		
-		for (int i = 0; i < 200; i++) {
-			Hashtable<String, String> insertion = new Hashtable<String,
-					  String>(); insertion.put("col1", "str");
-					  insertion.put("col2", "int"); insertion.put("col3",
-					  "int"); insertion.put("col4", "str");
-					  
-			  insertIntoTable("testAll2", insertion);
-		}
-		
+		// System.out.println(alreadyExist("test10"));
+
+		// makeTable("test4")
 		/*
-		 	Hashtable<String, String> insertion = new Hashtable<String,
-					  String>(); insertion.put("col1", "str");
-					  insertion.put("col2", "int"); insertion.put("col3",
-					  "int"); insertion.put("col4", "str");
-					  
-			  insertIntoTable("testAll2", insertion);
-			  */
-				Page x = (Page)deserialize("data/pages/testAll2/1.bin");
-				System.out.println(x.getRecords());
-				
-		
-		
+		 * FileInputStream fi = new FileInputStream("data/tables/test4.bin");
+		 * ObjectInputStream os = new ObjectInputStream(fi); Table x =
+		 * (Table)os.readObject(); System.out.println(x.getTableName());
+		 * os.close(); fi.close();
+		 */
+		/*
+		 * for (int i = 0; i < 200; i++) { Hashtable<String, String> insertion =
+		 * new Hashtable<String, String>(); insertion.put("col1", "str");
+		 * insertion.put("col2", "int"); insertion.put("col3", "int");
+		 * insertion.put("col4", "str");
+		 * 
+		 * insertIntoTable("testAll2", insertion); }
+		 */
+
+		Hashtable<String, String> insertion = new Hashtable<String, String>();
+		insertion.put("col1", "str");
+		insertion.put("col2", "int");
+		insertion.put("col3", "int");
+		insertion.put("col4", "str");
+
+		insertIntoTable("testAll6", insertion);
+
+		// Page x = (Page)deserialize("data/testAll/0.class");
+		// System.out.println(x.getRecords());
+
+		// --------------------------------------------------------------------
+
 	}
 }
 
 // createTable Done
 // createInsex Done
-// insertIntoTable Under Construction
+// insertIntoTable Done --> lssa 7war el key bs
--- src/Table.java
+++ src/Table.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -10,7 +9,6 @@ public class Table implements Serializable {
 	private int nameCounter;
 	private String pagesDirectory;
 	private String tableName;
-	
 
 	private static final long serialVersionUID = 1928828356132285922L;
 
@@ -18,54 +16,35 @@ public class Table implements Serializable {
 		this.allPages = new ArrayList<String>();
 		this.tableName = name;
 		this.nameCounter = 0;
-		pagesDirectory = "data/pages/"+tableName;
-		createFolder(pagesDirectory);
-	}
-	
-	public void createFolder(String x){
-		File saveDir = new File(x);
-		if(!saveDir.exists()){
-		    saveDir.mkdirs(); 
-		}
-	}
-/*
-	public void createPage() throws IOException {
-		Page x = new Page(tableName, "" + pagesNames.size());
-		File saveDir = new File("data/" + tableName);
-		if (!saveDir.exists()) {
-			saveDir.mkdirs();
-		}
-		FileOutputStream fs = new FileOutputStream("data/" + tableName + "/"
-				+ pagesNames.size() + ".bin");
-		ObjectOutputStream os = new ObjectOutputStream(fs);
-		os.writeObject(x);
-		os.close();
-		fs.close();
-		// pagesNames.add(x.);
+		pagesDirectory = "data/tables/" + tableName + "/pages";
 	}
 
-*/
-		
-	public  void createPage() throws IOException{
-		Page x = new Page(""+nameCounter);
-		String path = pagesDirectory+"/"+x.getPageName()+".bin";
+	/*
+	 * public void createPage() throws IOException { Page x = new
+	 * Page(tableName, "" + pagesNames.size()); File saveDir = new File("data/"
+	 * + tableName); if (!saveDir.exists()) { saveDir.mkdirs(); }
+	 * FileOutputStream fs = new FileOutputStream("data/" + tableName + "/" +
+	 * pagesNames.size() + ".bin"); ObjectOutputStream os = new
+	 * ObjectOutputStream(fs); os.writeObject(x); os.close(); fs.close(); //
+	 * pagesNames.add(x.); }
+	 */
+
+	public void createPage() throws IOException {
+		Page x = new Page("" + nameCounter);
+		String path = pagesDirectory + "/" + x.getPageName() + ".class";
 		FileOutputStream fs = new FileOutputStream(path);
 		ObjectOutputStream os = new ObjectOutputStream(fs);
 		os.writeObject(x);
 		os.close();
 		fs.close();
-		allPages.add(""+nameCounter);
+		allPages.add("" + nameCounter);
 		nameCounter++;
-		
+
 	}
-	
-	
 
 	public ArrayList<String> getAllPages() {
 		return allPages;
 	}
-
-	
 
 	public int getNameCounter() {
 		return nameCounter;
@@ -79,7 +58,6 @@ public class Table implements Serializable {
 		return serialVersionUID;
 	}
 
-	
 	public String getPagesDirectory() {
 		return pagesDirectory;
 	}
@@ -91,16 +69,14 @@ public class Table implements Serializable {
 	public String getTableName() {
 		return tableName;
 	}
+
 	public void setTableName(String tableName) {
 		this.tableName = tableName;
 	}
-	
-	public void addPagetoArray(Page x){
+
+	public void addPagetoArray(Page x) {
 		allPages.add(x.getPageName());
 	}
-	
-	
-
 
 	public static void main(String[] args) throws IOException {
 		new Table("Test2");
